49932b1d42c4202d7b8cee4868239ed8971da978
Ahasan     Rownok <ahsan.rownok@iisgalvanimi.edu.it>
2021-03-24 21:11:13 +0000

    chat

---
+++ client-socketio/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { SocketIoModule, SocketIoConfig } from 'ngx-socket-io';
+import { FormsModule } from '@angular/forms';
+import { SocketService } from './socket.service';
+
+const config: SocketIoConfig = { url: 'https://3000-rose-herring-ztt2nzrt.ws-eu03.gitpod.io', options: {/*transport : ['websocket'], withCredentials:false*/} };
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    SocketIoModule.forRoot(config),
+    FormsModule
+  ],
+  providers: [SocketService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
